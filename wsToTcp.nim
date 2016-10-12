1db45d735b57bc6101b7002bf20a0889abd90b11
ent <ent@example.com>
2016-10-12 23:52:31 +0200

    initial commit

---
+++ wsToTcp.nim
@@ -0,0 +1,83 @@
+## This is an asyn websocket to tcp transport,
+## Atm this is capable of tunneling a line based protocol (namely IRC)
+## Everything else is not tested.
+
+
+import asyncnet, asyncdispatch, websocket, asynchttpserver
+
+type Host = tuple[host: string, port: Port]
+
+proc pumpWsToTcp(req: Request, endpointSocket: AsyncSocket): Future[void] {.async.} =
+  var fromWs: tuple[opcode: Opcode, data: string]
+  while true:
+
+    try:
+      fromWs = await req.client.readData(false)
+    except:
+      return
+    echo "ws: " & fromWs.data
+    if fromWs.data == "":
+      raise
+
+    try:
+      await endpointSocket.send(fromWs.data)
+    except:
+      # break
+      raise
+
+proc pumpTcpToWs(req: Request, endpointSocket: AsyncSocket): Future[void] {.async.} =
+  var fromEndpoint: string = ""
+  while true:    
+    try:
+      fromEndpoint = await endpointSocket.recvLine()
+    except:
+      return
+
+    echo "endpoint: " & fromEndpoint
+    if fromEndpoint == "":
+      return
+
+    try:
+      await req.client.sendText(fromEndpoint,false)
+    except:
+      echo "pumpTcpToWs client socket is fuckd"
+      return
+
+proc processClientWebsocket(req: Request, src, dst: Host) {.async.} =
+  let (success, error) = await(verifyWebsocketRequest(req, "irc"))
+  if not success:
+    if req.url.path == "/":
+      echo "Deliver TWIRC - Tiny Web IRC Client"
+      asyncCheck req.respond(Http200, readFile("twirc.html") )
+    req.client.close()
+  else:
+    echo "New websocket customer arrived!"
+    var endpointSocket: AsyncSocket = newAsyncSocket()
+    try:
+      await endpointSocket.connect(dst.host, dst.port)
+      echo "Connected to endpoint"
+      asyncCheck pumpWsToTcp(req, endpointSocket)
+      asyncCheck pumpTcpToWs(req, endpointSocket) 
+    except:
+      echo "Could not connect to endpoint :("
+      asyncCheck req.client.sendText("Could not connect to endpoint :(\n",false)
+      echo "Closeing ws and tcp socket after error..."
+
+      req.client.close()
+      endpointSocket.close()
+
+proc proxy * (src, dst: Host ) =
+  ## Proxy line by line based protocols between ws://src and tcp://dst
+  var websocketServer = newAsyncHttpServer()
+  proc paramHelper(req: Request) {.async.} = 
+    try:
+      asyncCheck processClientWebsocket(req, src, dst) 
+    except:
+      echo "processClientWebsocket is fuckd"
+  asyncCheck websocketServer.serve(src.port, paramHelper)
+
+when isMainModule:
+  # proxy( ("0.0.0.0",Port(7787)), ("127.0.0.1",Port(6667)) )
+  proxy( ("0.0.0.0",Port(7787)), ("irc.freenode.net",Port(6667)) )
+  runForever()
+  
